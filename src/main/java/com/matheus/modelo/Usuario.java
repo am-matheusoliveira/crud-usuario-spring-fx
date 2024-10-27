b3e35facebf411d358a0492398faeff2ee47ae30
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-10-27 13:37:43 -0300

    Adiciona classe Usuario.java como entidade modelo

---
+++ src/main/java/com/matheus/modelo/Usuario.java
@@ -0,0 +1,92 @@
+package com.matheus.modelo;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String data_nascimento;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    @Column(name = "data_cadastro", updatable = false, insertable = false)
+    private LocalDateTime data_cadastro;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getDataNascimento() {
+        return data_nascimento;
+    }
+
+    public void setDataNascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return data_cadastro;
+    }
+}
